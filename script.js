391207be10c1cb118192d8db6351783f8187f3de
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-03-01 03:19:30 -0300

    adicionado funções para salvar no local storage que é o navegador do usuário

--- script.js
+++ script.js
@@ -1,72 +1,78 @@
-// Array com as tarefas
 let listaTarefas = [];
 
-// Seleciona o elemento ul da lista de tarefas
-const listaTarefasElement = document.querySelector('#lista-de-tarefas');
-
-// Adiciona um evento de clique ao botão adicionar
-document.querySelector('#botao-adicionar').addEventListener('click', adicionarTarefa);
-
-// Adiciona um evento de tecla pressionada ao campo de texto
-document.querySelector('#nova-tarefa').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        adicionarTarefa();
-    }
-});
-
-// Adiciona a tarefa à lista
 function adicionarTarefa() {
-    const inputTarefa = document.querySelector('#nova-tarefa');
-    const tarefa = inputTarefa.value.trim();
+    const novaTarefaElement = document.getElementById('nova-tarefa');
+    const tarefa = novaTarefaElement.value.trim();
 
     if (tarefa !== '') {
         listaTarefas.push(tarefa);
+        salvarListaLocalStorage();
         atualizarLista();
-        inputTarefa.value = '';
+        novaTarefaElement.value = '';
     }
 }
 
-// Remove a tarefa da lista
-function removerTarefa(index) {
-    listaTarefas.splice(index, 1); // Remove a tarefa do array
-    atualizarLista(); // Atualiza a lista de tarefas
+function removerTarefa(i) {
+    listaTarefas.splice(i, 1);
+    salvarListaLocalStorage();
+    atualizarLista();
 }
 
-// Edita a tarefa da lista
-function editarTarefa(index) {
-    const itemLista = listaTarefasElement.children[index];
-    const tarefa = listaTarefas[index];
+function editarTarefa(i) {
+    const lista = document.getElementById('lista-de-tarefas');
+    const itemLista = lista.children[i];
+    const tarefa = listaTarefas[i];
 
     itemLista.innerHTML = `
-        <input type="text" class="form-control mr-2" value="${tarefa}" id="editar-tarefa-${index}">
-        <button class="btn btn-sm btn-outline-success" onclick="salvarTarefa(${index})"><img src="img/salvar.svg" alt="Salvar" /></button>
-        <button class="btn btn-sm btn-outline-secondary" onclick="atualizarLista()"><img src="img/cancelar.svg" alt="Cancelar" /></button>
-    `;
+    <input type="text" class="form-control mr-2" value="${tarefa}" id="editar-tarefa-${i}">
+    <button class="btn btn-sm btn-outline-success" onclick="salvarTarefa(${i})"><img src="img/salvar.svg" alt="Salvar" /></button>
+    <button class="btn btn-sm btn-outline-secondary" onclick="atualizarLista()"><img src="img/cancelar.svg" alt="Cancelar" /></button>
+  `;
 }
 
-// Salva a tarefa editada
-function salvarTarefa(index) {
-    const inputEditarTarefa = document.querySelector(`#editar-tarefa-${index}`);
-    const tarefa = inputEditarTarefa.value.trim();
+function salvarTarefa(i) {
+    const inputEditarTarefaElement = document.querySelector(`#editar-tarefa-${i}`);
+    const tarefa = inputEditarTarefaElement.value.trim();
 
     if (tarefa !== '') {
-        listaTarefas[index] = tarefa;
+        listaTarefas[i] = tarefa;
+        salvarListaLocalStorage();
         atualizarLista();
     }
 }
 
-// Atualiza a lista de tarefas
 function atualizarLista() {
-    listaTarefasElement.innerHTML = '';
+    const listaElement = document.getElementById('lista-de-tarefas');
+    listaElement.innerHTML = '';
 
-    listaTarefas.forEach((tarefa, index) => {
+    listaTarefas.forEach((tarefa, i) => {
         const itemLista = document.createElement('li');
         itemLista.className = 'list-group-item d-flex justify-content-between align-items-center';
         itemLista.innerHTML = `${tarefa}
-            <div>
-                <button class="btn btn-sm btn-outline-primary mr-2" onclick="editarTarefa(${index})"><img src="img/editar.svg" alt="editar" /></button>
-                <button class="btn btn-sm btn-outline-danger" onclick="removerTarefa(${index})"><img src="img/remover.svg" alt="remover" /></button>
-            </div>`;
-        listaTarefasElement.appendChild(itemLista);
+      <div>
+        <button class="btn btn-sm btn-outline-primary mr-2" onclick="editarTarefa(${i})"><img src="img/editar.svg" alt="editar" /></button>
+        <button class="btn btn-sm btn-outline-danger" onclick="removerTarefa(${i})"><img src="img/remover.svg" alt="remover" /></button>
+      </div>`;
+        listaElement.appendChild(itemLista);
     });
 }
+
+function salvarListaLocalStorage() {
+    localStorage.setItem('listaTarefas', JSON.stringify(listaTarefas));
+}
+
+function carregarListaLocalStorage() {
+    const listaLocalStorage = JSON.parse(localStorage.getItem('listaTarefas'));
+
+    if (listaLocalStorage !== null) {
+        listaTarefas = listaLocalStorage;
+        atualizarLista();
+    }
+}
+
+carregarListaLocalStorage();
+
+document.getElementById('formulario').addEventListener('submit', function (e) {
+    e.preventDefault();
+    adicionarTarefa();
+});
